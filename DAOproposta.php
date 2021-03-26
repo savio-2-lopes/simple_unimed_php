3b1af8259ab520afabb2e74ea7c67f2f56e5f174
SAVIO AUGUSTO LOPES <savio.lopes@fatec.sp.gov.br>
2021-03-26 09:27:32 -0300

    :memo: Update template

--- DAOproposta.php
+++ DAOproposta.php
@@ -12,7 +12,7 @@ class PropostaDAO
     public function cadastra($nome, $idade, $plano, $total)
     {
         $sql = "INSERT INTO unimed (nome,idade,plano,total) VALUES ('$nome','$idade','$plano','$total')";
-        $executa = mysqli_query($this->conexao->getCon(), $sql);
+        $ex = mysqli_query($this->conexao->getCon(), $sql);
 
         if (mysqli_affected_rows($this->conexao->getCon()) > 0) {
             return true;
